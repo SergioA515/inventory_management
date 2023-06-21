4406693385cd82bacdd89f18a709eb21e9868d52
Sergio Alejandro <alejoparra191@gmail.com>
2023-06-21 01:54:04 -0500

    small changes

--- helpers/utils.php
+++ helpers/utils.php
@@ -2,7 +2,7 @@
 require_once('config/params.php');
 // Obtén la URL solicitada
 function requireClassFile($className) {
-    $classFile = url_init . '/controllers/' . $className . '.php';
+    $classFile = url_init . $className . '/controllers/' . '.php';
     if (file_exists($classFile)) {
         require_once $classFile;
     }
@@ -50,4 +50,46 @@ if (array_key_exists($url, $routes)) {
 } else {
     // Ruta no encontrada, mostrar un error o redirigir a una página de error
     echo 'Error: Ruta no encontrada';
+}
+
+/* Vamos a disponer de esta información para su uso eventual:
+// Función para verificar y redireccionar la URL
+function redirect($url) {
+    // Rutas disponibles y sus correspondientes controladores y acciones
+    $routes = [
+        '/productos' => ['ProductoController', 'verProductos'], ['ProductoController', 'edicionProductos'],
+        '/mayoristas' => ['MayoristaController', 'verMayoristas'],
+        '/pedidos' => ['PedidoController', 'verPedidos']
+        // Agrega más rutas según tus necesidades
+    ];
+
+    // Verifica si la ruta solicitada existe en las rutas disponibles
+    if (array_key_exists($url, $routes)) {
+        // Obtiene el controlador y la acción correspondientes a la ruta
+        [$controllerName, $action] = $routes[$url];
+
+        // Requerir el archivo del controlador
+        requireClassFile($controllerName);
+
+        // Verificar si la clase del controlador existe
+        if (class_exists($controllerName)) {
+            // Crear una instancia del controlador
+            $controller = new $controllerName();
+
+            // Verificar si la acción existe en el controlador
+            if (method_exists($controller, $action)) {
+                // Llamar a la acción del controlador
+                $controller->$action();
+            } else {
+                // Acción no encontrada, mostrar un error o redirigir a una página de error
+                echo 'Error: Acción no encontrada';
+            }
+        } else {
+            // Controlador no encontrado, mostrar un error o redirigir a una página de error
+            echo 'Error: Controlador no encontrado';
+        }
+    } else {
+        // Ruta no encontrada, mostrar un error o redirigir a una página de error
+        echo 'Error: Ruta no encontrada';
+    }
 }
--- index.php
+++ index.php
@@ -5,4 +5,17 @@ require_once('config/Conexion.php');
 require_once('views/ingreso.php');
 require_once('helpers/utils.php');
 
+/* Vamos a disponer de esta información para su uso eventual:
+// Obtén la URL solicitada
+$url = $_SERVER['REQUEST_URI'];
 
+// Elimina cualquier parámetro de la URL
+$url = strtok($url, '?');
+
+// Requerir el archivo 'utils.php' para tener acceso a las funciones y rutas
+require_once('helpers/utils.php');
+
+// Verificar y redireccionar la URL
+redirect($url);
+*/
+require_once('views/includes/layouts/footer_standar.php');
